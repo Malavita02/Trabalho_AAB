9cea8d94d2d655e2898ba893b42e4aa5822bf3e8
ruteeecastrooo <100296344+ruteeecastrooo@users.noreply.github.com>
2022-05-13 15:20:12 +0100

    Update BoylerMoore.py

--- BoylerMoore.py
+++ BoylerMoore.py
@@ -3,31 +3,29 @@ class BoyerMoore:
     '''
         O objetivo desta class é encontrar onde o pattern se repete numa determinada sequência/texto.
         A função contrutora recebe o padrão e o alfabeto a que o mesmo pertence.
-
         Inputs:
             :pattern: Padrão a procurar
             :type pattern: string
             :alphabet: Alfabeto que o padrão pertence (por omissão "ATCG")
             :type alphabet: string
-
     '''
     def __init__(self, pattern, alphabet = "ATCG"):
-        self.alphabet = alphabet
         self.pattern = pattern
+        self.alphabet = alphabet
         self.preprocess()
 
     def preprocess(self):
-        self.process_bcr()
-        self.process_gsr()
+        self.process_bad_character_rule()
+        self.process_good_suffix_rule()
 
-    def process_bcr(self):
-        self.occ = {}
+    def process_bad_character_rule(self):
+        self.occurrences = {}
         for s in self.alphabet:
-            self.occ[s] = -1
-        for j in range(len(self.pattern)):
-            self.occ[self.pattern[j]] = j
-
-    def process_gsr(self):
+            self.occurrences[s] = -1
+        for j in range(len(self.pattern)): # AATTTCCG occ['A'] = 1, occ['T'] = 4, occ['C'] = 6, occ['G'] = 7
+            self.occurrences[self.pattern[j]] = j
+            
+     def process_good_suffix_rule(self):
         self.f = [0 for n in range(len(self.pattern) + 1)]
         self.s = [0 for n in range(len(self.pattern) + 1)]
         i = len(self.pattern)
@@ -47,11 +45,10 @@ class BoyerMoore:
                 self.s[i] = j
             if i == j:
                 j = self.f[j]
-
-    def search_pattern(self, text):
+                
+     def search_pattern(self, text):
         '''
             Recebe a sequência ou o texto onde se pertende encontrar o padrão e devolve uma lista com os indices onde se repete o padrão
-
             Inputs:
                 :text: Sequência ou o texto para procurar o padrão
                 :type text: string
@@ -63,12 +60,12 @@ class BoyerMoore:
         i = 0
         while i <= len(text) - len(self.pattern):
             j = len(self.pattern) - 1
-            while j >= 0 and self.pattern[j] == text[j + i]:
+            while j >= 0 and self.pattern[j] == text[i + j]:
                 j -= 1
             if j < 0:
                 res.append(i)
                 i += self.s[0]
             else:
-                c = text[j + i]
-                i += max(self.s[j + 1], j - self.occ[c])
+                c = text[i + j]
+                i += max(self.s[j + 1], j - self.occurrences[c])
         return res
