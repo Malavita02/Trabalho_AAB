c7d237d3808967ce930a500076ff6f284b81ddef
Tiago Oliveira <86690221+Malavita02@users.noreply.github.com>
2022-05-23 19:53:51 +0100

    Add files via upload

---
+++ BoyerMoore_test.py
@@ -0,0 +1,15 @@
+import unittest
+from BoyerMoore import BoyerMoore
+
+class TestBoyerMoore(unittest.TestCase):
+    def test_search_pattern(self):
+        bm = BoyerMoore("ACCA", "ACTG")
+        self.assertEqual(bm.search_pattern("ATAGAACCAATGAACCATGATGAACCATGGATACCCAACCACC"), [5, 13, 23, 37])
+        bm = BoyerMoore("AGACG", "ACTG")
+        self.assertEqual(bm.search_pattern("ATAGACGGTATTTGTCCATACAAAGGTAGACGTTATGGAACCTGTC"), [2, 27])
+
+    def test_types(self):
+        bm = BoyerMoore("AGACG", "ACTG")
+        self.assertRaises(TypeError, bm.__innit__, "phge")
+
+
