5b5add28d0a1a38b5844e48b245fec8098962a3d
RubenPTFCP <96090016+RubenPTFCP@users.noreply.github.com>
2022-05-24 16:43:18 +0100

    Update SufixTrie.py

--- SufixTrie.py
+++ SufixTrie.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 class Sufix_trie:
     """
-
+    Esta classe permite fazer um pré-processamento da sequência na qual se quer procurar o padrão (ou padrões)
     """
     def __init__(self, seq):
         n = len(seq)
@@ -23,6 +23,11 @@ class Sufix_trie:
         t["$"] = i
 
     def matches(self, seq):
+        '''
+        Esta função procura o grau do nodo e se não existir retorna falso
+        Inputs:
+            :seq: Padrão da sequência
+        '''
         t = self.trie
         for x in seq:
             if x not in t: return False
