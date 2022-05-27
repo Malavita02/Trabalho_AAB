9c520c72b97f3ca3ea6775a77ffbdcd0e1da669e
Ruben <96090016+RubenPTFCP@users.noreply.github.com>
2022-05-27 21:41:36 +0100

    Update debruijn.py

--- debruijn.py
+++ debruijn.py
@@ -3,12 +3,22 @@
 from MyGraph import MyGraph
 
 class DeBruijnGraph (MyGraph):
+    '''
+    Esta classe representa os fragmentos (k-mers) não como nós do grafo mas antes como arcos, sendo os nós sequências 
+    de tamanho k-1 correspondendo a prefixos/ sufixos destes fragmentos.
+    '''
     
     def __init__(self, frags):
         MyGraph.__init__(self, {})
         self.create_deBruijn_graph(frags)
 
     def add_edge(self, o, d):
+        '''
+        Método add_edge redefinido para admitir arcos repetidos
+        Inputs:
+            :o:
+            :d:
+        '''
         if o not in self.graph.keys():
             self.add_vertex(o)
         if d not in self.graph.keys():
@@ -37,10 +47,28 @@ class DeBruijnGraph (MyGraph):
             seq += nxt[-1]
         return seq 
     
-def suffix (seq): 
+def suffix (seq):
+    '''
+    Dá o sufixo da sequência
+    Inputs:
+        :seq: Sequência
+        :type seq: str
+    Returns:
+        :return int: sufixo da sequência
+        :rtype int: int
+    '''
     return seq[1:]
     
 def prefix(seq):
+    '''
+    Dá o prefixo da sequência
+    Inputs:
+        :seq: Sequência
+        :type seq: str
+    Returns:
+        :return int: prefixo da sequência
+        :rtype int: int
+    '''
     return seq[:-1]
 
 def composition(k, seq):
